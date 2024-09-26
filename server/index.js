755de246be99809bb16094393a970fa569898daa
Mario Alvarez <Malvar322@west-mec.org>
2024-09-26 14:20:58 -0700

    FINAL complete Super Squad Project

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+// index.js
+// Required modules
 const express = require('express');
 const path = require('path');
 const fs = require('fs').promises;
@@ -27,12 +29,12 @@ app.get('/users', async (req, res) => {
 
         const users = JSON.parse(data);
         if (!users) {
-            throw new Error("Error no superheros available");
+            throw new Error("Error no users available");
         }
         res.status(200).json(users);
     } catch (error) {
-        console.error("Problem getting superheros" + error.message);
-        res.status(500).json({ error: "Problem reading superheros" });
+        console.error("Problem getting users" + error.message);
+        res.status(500).json({ error: "Problem reading users" });
     }
 });
 
@@ -53,7 +55,7 @@ app.post('/submit-form', async (req, res) => {
             users = JSON.parse(data);
         } catch (error) {
             // If file doesn't exist or is empty, start with an empty array
-            console.error('Error reading superhero data:', error);
+            console.error('Error reading user data:', error);
             users = [];
         }
 
@@ -88,7 +90,7 @@ app.put('/update-user/:currentName/:currentEmail', async (req, res) => {
             const userIndex = users.findIndex(user => user.name === currentName && user.email === currentEmail);
             console.log(userIndex);
             if (userIndex === -1) {
-                return res.status(404).json({ message: "Superhero not found" })
+                return res.status(404).json({ message: "User not found" })
             }
             users[userIndex] = { ...users[userIndex], name: newName, email: newEmail };
             console.log(users);
@@ -97,11 +99,47 @@ app.put('/update-user/:currentName/:currentEmail', async (req, res) => {
             res.status(200).json({ message: `You sent ${newName} and ${newEmail}` });
         }
     } catch (error) {
-        console.error('Error updating superhero:', error);
-        res.status(500).send('An error occurred while updating the superhero.');
+        console.error('Error updating user:', error);
+        res.status(500).send('An error occurred while updating the user.');
     }
 });
-
+app.delete('/user/:name/:email', async (req, res) => {
+    try {
+        // console.log req.params
+        // console.log(req.params);
+        // then cache returned name and email
+        // as destructured variables from params
+        // console.log(req.params.name);
+        // console.log(req.params.email);
+        const { name, email } = req.params
+        // initalize an empty array of 'users'
+        let users = [];
+        // try to read the users.json file and cache as data
+        try {
+            const data = await fs.readFile(dataPath, 'utf8');
+            users = JSON.parse(data);
+        } catch (error) {
+            return res.status(404).send('User data not found')
+        }
+        // cache the userIndex based on a matching name and email
+        const userIndex = users.findIndex(user => user.name === name && user.email === email);
+        console.log(userIndex);
+        if (userIndex === -1) {
+            return res.status(404).send('User not found');
+        }
+        // splice the users array with the intended delete name and email
+        users.splice(userIndex, 1);
+        try {
+            await fs.writeFile(dataPath, JSON.stringify(users, null, 2));
+        } catch (error) {
+            console.error("Failed to write to database");
+        }
+        // send a success deleted message
+        res.send('User deleted successfully');
+    } catch (error) {
+        res.status(500).send('There was an error deleting user');
+    }
+});
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
